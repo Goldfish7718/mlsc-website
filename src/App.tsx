e7131678d01c9cdd629be844250a0c5e56f0d1a3
Tejas Nanoti <tejasnanoti2@gmail.com>
2025-02-14 09:03:17 +0530

    fix: Reinitialize shadcn & tailwind v4 config;

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-extrabold'>
+        MLSC WEB
+      </h1>
+
+      <Button>Shadcn Button</Button>
+    </>
+  )
+}
+
+export default App
